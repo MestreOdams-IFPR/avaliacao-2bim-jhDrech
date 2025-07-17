8409af89e25d7774f6d9576b5a0c5691c0c2c5fa
OdairMoreira <odair.moreira.souza>
2025-07-17 15:37:46 -0300

    Conceito A | Prof. Odair

--- SetencaDancante.java
+++ SetencaDancante.java
@@ -8,20 +8,20 @@ public class SetencaDancante {
         arrayPalavra[0] = Character.toUpperCase(arrayPalavra[0]);
         boolean maiusculo = true;
         for (int i = 1; i < arrayPalavra.length; i++) {
-            if (!maiusculo && (int)arrayPalavra[i] != 32) {
+            if (!maiusculo && (int) arrayPalavra[i] != 32) {
                 arrayPalavra[i] = Character.toUpperCase(arrayPalavra[i]);
                 maiusculo = true;
-            }
-            else if((int)arrayPalavra[i] != 32) {
+            } else if ((int) arrayPalavra[i] != 32) {
                 maiusculo = false;
             }
-        } 
+        }
         String palavraDancante = "";
         for (int i = 0; i < arrayPalavra.length; i++) {
             palavraDancante += arrayPalavra[i];
         }
         return palavraDancante;
     }
+
     public static void main(String[] args) {
         String palavra = sc.nextLine();
         String palavraDancante = transformarEmDancante(palavra);
